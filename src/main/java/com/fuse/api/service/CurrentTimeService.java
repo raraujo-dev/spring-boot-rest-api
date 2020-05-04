306017e4e0bdd2bace1ffa5d71316c8548dedfc4
raraujo <raraujo@localhost>
2020-05-04 00:12:59 -0300

    Inserting Spring Profiles

--- src/main/java/com/fuse/api/service/CurrentTimeService.java
+++ src/main/java/com/fuse/api/service/CurrentTimeService.java
@@ -3,15 +3,19 @@ package com.fuse.api.service;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CurrentTimeService {
 	
+	@Autowired
+	Environment env;
 	
 	public String getCurrentDateTime() {
 		
-		String message = "Hello from DEV environment and the current date time is ";
+		String message = env.getProperty("message");
 		
 		LocalDateTime localDateTime = LocalDateTime.now();
 		
